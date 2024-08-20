6b90e072e101a1c32073576fa98e18038ad6986b
JonatasTB <101901086+jonatastb@users.noreply.github.com>
2024-08-20 01:25:19 -0300

    :sparkles: feat: solution published

---
+++ SolutionService.php
@@ -0,0 +1,30 @@
+<?php
+
+class Solution {
+
+  /**
+   * @param String $word
+   * @return Boolean
+   */
+  function detectCapitalUse($word) {
+
+      $areAllLettersUppercase = ctype_upper($word);
+      if ($areAllLettersUppercase) {
+          return true;
+      }
+      
+      $areAllLettersLowercase = ctype_lower($word);
+      if ($areAllLettersLowercase) {
+          return true;
+      }
+      
+      $firstLetterInWordAreUppercase = ctype_upper($word[0]);
+      $isTheWordWithoutTheFirstLetterStillEntirelyLowercase = ctype_lower(substr($word, 1));
+      
+      if ($firstLetterInWordAreUppercase && $isTheWordWithoutTheFirstLetterStillEntirelyLowercase) {
+          return true;
+      }
+
+      return false;
+  }
+}
